b589af0f330d92685bae77e7cdae4ce0b70d1fa5
sergeyvakhrin <parlamentsv@mail.ru>
2024-12-22 21:28:33 +0300

    add Userserializer def get_users_list

--- users/serliazers.py
+++ users/serliazers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Referral
 
 
 class PhoneSerializer(serializers.Serializer):
@@ -9,7 +9,16 @@ class PhoneSerializer(serializers.Serializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    users_list = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = '__all__'
+
+    def get_users_list(self, instance):
+        """
+        Выводит список пользователей (номера телефонов),
+        которые ввели реферальный код текущего пользователя.
+        """
+        users_list = User.objects.filter(user_referral=instance.self_referral)
+        return [user.phone for user in users_list]
